18aa46de40aecdb6e919aabefd37b91179a3b8cb
shaikshanu123 <jesusaman49@gmail.com>
2026-02-12 12:17:45 +0530

    Add files via upload

---
+++ ds.py
@@ -0,0 +1,35 @@
+
+# value = input("Enter something: ")
+# name = input("Enter your name: ")
+# print("Hello,", name)
+#input()
+# age = int(input("Enter your age: "))
+# print("You are", age, "years old.")
+# price = float(input("Enter price: "))
+# print("Final Price =", price)
+# a, b = map(int, input("Enter two numbers: ").split())
+# print(a + b)
+# x, y, z = map(float, input("Enter three decimals: ").split())
+# print(x, y, z)
+# nums = list(map(int, input("Enter numbers: ").split()))
+# print(nums)
+# words = input("Enter words: ").split()
+# print(words)
+# data = input("Enter values: ").split(",")
+# print(data)
+# x = eval("10 + 20")
+# print(x)    
+# result = eval(expr)
+# print("Result =", result)
+# lst = eval(input("Enter a list: "))
+# name = input("Enter name: ")
+# if name == "admin":
+#     print("Welcome Admin!")
+# else:
+#     print("Hello User!")
+
+# text = "   hello world   "
+
+print(text.strip())   # "hello world"
+print(text.lstrip())  # "hello world   "
+print(text.rstrip())  # "   hello world"
